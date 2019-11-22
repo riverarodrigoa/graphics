0764cf59dadea1b2128e270e58181957f83bf51a
Rodrigo Rivera <rod2693rm@gmail.com>
2019-11-22 21:08:53 +0100

    Update plot function.

--- graphics/graphics.py
+++ graphics/graphics.py
@@ -7,7 +7,9 @@ import matplotlib.ticker as ticker
 import seaborn as sns
 
 
-def plot_comp_all_vars(da, vars_comp, start=None, end=None, qq=(0.0, 1.0), sec=None, ylabs=None, legend_labs=None, ylims=None, mask_date=None, vline=None, resolution="d", file_name=None, figsize=(30, 23), alpha=1.0, fontsize=14, interplotspace=(None, None)):
+def plot_comp_all_vars(da, vars_comp, start=None, end=None, qq=(0.0, 1.0), sec=None, ylabs=None, legend_labs=None,
+                       ylims=None, mask_date=None, vline=None, resolution="D", file_name=None, figsize=(30, 23),
+                       alpha=1.0, fontsize=14, interplotspace=(None, None), comp_in_subplot=False):
     sns.set(font_scale=1.3)
     if start is None:
         start = da.index[0]
@@ -34,6 +36,9 @@ def plot_comp_all_vars(da, vars_comp, start=None, end=None, qq=(0.0, 1.0), sec=N
     n = len(ylabs)
     # c = d.columns
     colors = plt.cm.get_cmap('tab10')(np.linspace(0, 1, n + 1))
+    if comp_in_subplot:
+        c = len(max(vars_comp))
+        cmp_colors = plt.cm.get_cmap('Set1')(np.linspace(0, 1, c))
     # colors2 = plt.cm.get_cmap('tab10')(np.linspace(0, 1, len(c)))
     with plt.style.context('seaborn-whitegrid'):
         fig, ax = plt.subplots(nrows=n, sharex=True, figsize=figsize, squeeze=False)
@@ -47,11 +52,15 @@ def plot_comp_all_vars(da, vars_comp, start=None, end=None, qq=(0.0, 1.0), sec=N
             else:
                 ax[i, 0] = d.loc[:, vars_comp[i]].plot(ax=ax[i, 0], style='.', cmap=colors2, grid=True, xticks=xticks.to_list(), rot=0, ms=2, alpha=alpha)
 
-            if i == 0:
-                ax[i, 0].lines[0].set_color('r')
+            if comp_in_subplot:
+                [ax[i, 0].lines[k].set_color(color) for k, color in enumerate(cmp_colors)]
+
             else:
-                if i != 1:
-                    ax[i, 0].lines[0].set_color(colors[i + 1])
+                if i == 0:
+                    ax[i, 0].lines[0].set_color('r')
+                else:
+                    if i != 1:
+                        ax[i, 0].lines[0].set_color(colors[i + 1])
 
             if legend_labs is not None:
                 ax[i, 0].legend(legend_labs[i], markerscale=5, prop={'size': fontsize}, loc='center left', bbox_to_anchor=(1, 0.5))
@@ -82,7 +91,11 @@ def plot_comp_all_vars(da, vars_comp, start=None, end=None, qq=(0.0, 1.0), sec=N
                 for k in vline:
                     ax[i, 0].axvline(x=k, color='r', linestyle='--')
             if i == n - 1:
-                ax[i, 0].set_xticklabels(xticks.strftime('%d-%b').tolist(), horizontalalignment='center', fontsize=fontsize)
+                if "H" in resolution:
+                    ax[i, 0].set_xticklabels(xticks.strftime('%d-%b %H:%M').tolist(), horizontalalignment='center', fontsize=fontsize)
+                else:
+                    ax[i, 0].set_xticklabels(xticks.strftime('%d-%b').tolist(), horizontalalignment='center', fontsize=fontsize)
+                ax[i, 0].tick_params(axis='x', pad=10)
             else:
                 ax[i, 0].set_xticklabels('')
 
